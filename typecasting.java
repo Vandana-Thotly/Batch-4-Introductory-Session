3995b8096f3889d89230f3344be8afdeefe79e68
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 20:09:27 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,25 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int c=10;
+	    int d=11;
+	    int e=12;
+	    int v1=5;
+	    double v2=v1; //widening
+	    double b=7.777;
+	    int a=(int) b; //manual
+	    c+=3;
+	    d-=4;
+	    e*=5;
+	    System.out.println(c);
+	    System.out.println(d);
+	    System.out.println(e);
+	    System.out.println(a);
+	    System.out.println(v2);
+	    System.out.println(Math.max());
+	    System.out.println();
+	    System.out.println();
+	    System.out.println();
+	    
+	}
+}
