4d2d3bfda16448d471e59e0c3a99d5d7dcad95d5
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 19:07:30 +0530

    Create js5.js

---
+++ js5.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    
+    <body>
+        <h2>Vandana Brief Profile</h2>
+        <p id="demo"></p>
+        <script>
+            let a='Hii my name is "vandana"';
+            let b='I am studying btech in "JNTUACEK"';
+            let std="My date of birth is"
+            let c=8+"April"
+            document.getElementById("demo").innerHTML=a+"<br>"+b+"<br>"+std+"&nbsp"+c;
+        </script>
+    </body>
+</html>
