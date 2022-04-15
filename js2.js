835f055256f044a4f1033fb200bc3e45cf90f2a1
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 19:04:47 +0530

    Create js2.js

---
+++ js2.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    
+    <body>
+        <h1>Welcome to first java script</h1>
+        <button type="button" onclick="document.getElementById('demo').innerHTML=Date()">click me to see date and type</button>
+        <p id="demo">Hii i am vandana</p>
+    </body>
+</html>
