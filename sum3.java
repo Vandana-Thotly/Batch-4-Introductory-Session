69a9bc9b0adf5d44ffe175acbf06b1adf8ce8fba
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 20:10:08 +0530

    Create sum3.java

---
+++ sum3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        System.out.println("enter 3 numbers"); 
+        Scanner sc=new Scanner(system.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=a+b+c;
+        System.out.println(d);
+    }
+}
