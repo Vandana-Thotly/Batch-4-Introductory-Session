63664404146108a1b52e99aed138cbd0765c296c
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 19:09:16 +0530

    Create js6.js

---
+++ js6.js
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    
+    <body>
+        <h2>Vandana Brief Profile</h2>
+        <p id="demo"></p>
+        <script>
+            const name=["vandana","saraswathi","niharika"]
+            document.getElementById("demo").innerHTML=name[1];
+        </script>
+    </body>
+</html>
