f238e8b8909175d37d44312949dc97e0145a7750
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 20:08:43 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,18 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int num=3;
+	    switch(num)
+	    {
+	        case 1:
+	            System.out.println("Vandana");
+	            break;
+	        case 2:
+	            System.out.println("saraswathi");
+	            break;
+	        case 3:
+	            System.out.println("niharika");
+	            break;
+	    }
+	}
+}
