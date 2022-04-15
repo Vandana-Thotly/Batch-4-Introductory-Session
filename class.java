6d96914dad3f440a9dd3f0f2f2dc32aa0ef02f60
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 20:00:26 +0530

    Create class.java

---
+++ class.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    int a=3;
+	public static void main(String[] args) {
+	    Main v1=new Main();
+	    Main v2=new Main();
+	    System.out.println(v1.a);
+	    System.out.println(v2.a);
+	    
+	}
+}
