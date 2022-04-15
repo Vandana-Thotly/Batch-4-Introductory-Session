531999a12ea1e5aeda8d88fc135069741e93ef2a
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 19:03:58 +0530

    Create javascript1.js

---
+++ javascript1.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script>
+            function v1()
+            {
+                
+                document.getElementById("demo").innerHTML="I am Saraswathi....";
+            }
+        </script>
+    </head>
+    <body>
+        <h1>Welcome to first java script</h1>
+        <p id="demo">Hii i am vandana</p>
+        <button type="button" onclick="v1()">click me</button>
+    </body>
+</html>
