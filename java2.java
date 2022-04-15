b77f6a94778ec3cc6954eb4adfb24177c25a40ac
Vandana-Thotly <71267341+Vandana-Thotly@users.noreply.github.com>
2022-04-15 19:55:31 +0530

    Create java2.java

---
+++ java2.java
@@ -0,0 +1,14 @@
+public class Main {
+    int i=2;
+    public static void main(String[] args){
+        Main v1=new Main();
+        Main v2=new Main();
+        if(v1.i>0){
+            System.out.println("positive");
+            
+        }
+        else if(v2.i<0){
+            System.out.println("Negative");
+        }
+    }
+}
